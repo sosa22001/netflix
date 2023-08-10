8d9ce29e8bd889aa249c7659046214ddc0fff90d
Elmer <sosa66093@gmail.com>
2023-08-10 01:21:16 -0600

    Uniendo backend con frontend

--- app/Http/Controllers/AutenticacionController.php
+++ app/Http/Controllers/AutenticacionController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 
 class AutenticacionController extends Controller
 {
@@ -12,9 +14,30 @@ class AutenticacionController extends Controller
 
     public function verificarUsuario(Request $request){
         //al verificar con el backend
-        //si todo está bien
-        //lo redirijo a los perfiles.
-        return $request;
+        $cliente = new Client();
+
+        $headers = [
+            'Content-Type' => 'application/json'
+        ];
+
+        $body = '{
+            "correo": "' . $request->input('email') . '",
+            "contrasena": "' . $request->input('contraseña') . '"
+        }';
+
+        $resultado = $cliente->get('http://localhost:8080/api/usuario',[
+            'headers' => $headers,
+            'body' => $body
+        ]);
+        
+        $usuario = json_decode($resultado->getBody(), true);
+
+        if($usuario ==null){
+            return "No existe el usuario";
+        }else{
+            return redirect()->route('perfiles.mostrar', ['idUsuario' => $usuario['idUsuario']]);
+        }
+        
     }
 
     public function mostrarRegistro(){
